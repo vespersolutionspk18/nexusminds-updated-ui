f21613ddc7a3125509fb86174fd33eab08522989
Vesper Solutions <vespersolutionspk@gmail.com>
2024-09-16 14:43:03 +0500

    forth commit

--- .history/components/SplineComponent_20240906141955.tsx
+++ .history/components/SplineComponent_20240906141955.tsx
@@ -3,7 +3,12 @@ import Spline from '@splinetool/react-spline/next';
 
 const SplineComponent = () => {
   return (
-    <></>
+    <Spline
+      scene="https://prod.spline.design/tfiaMWCfTnu1e1KB/scene.splinecode"
+      width={512}
+      height={512}
+      className="md:scale-125 md:flex hidden"
+    />
   );
 };
 
