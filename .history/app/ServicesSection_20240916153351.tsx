3d1e088aa5a4ae8fdbffa53cff6dfca9c746ac90
Vesper Solutions <vespersolutionspk@gmail.com>
2024-09-16 15:35:12 +0500

    seventh commit

--- .history/app/ServicesSection_20240916153351.tsx
+++ .history/app/ServicesSection_20240916153351.tsx
@@ -17,14 +17,14 @@ const ServicesSection = () => {
         <div className=" rounded-2xl md:ml-10 flex flex-col md:flex-row items-start justify-between pt-10">
         <div className="md:w-1/2"><h3 className="md:pl-10 font-semibold md:text-3xl text-2xl text-slate-800 px-10">Expertise</h3></div>
         <div className="md:w-1/2 w-full flex flex-row  md:pr-24 items-start justify-start gap-0 mt-5 md:mt-0 px-10 md:px-0">
-            <div className="flex flex-col gap-3  w-1/2 h-full items-start">
+            <div className="flex flex-col gap-3  w-1/2 h-full items-start ">
                 <h4 className="font-medium text-lg md:text-2xl underline text-slate-800 hover:text-purple-700">Software Development</h4>
                 <h4 className="font-medium text-lg md:text-2xl underline text-slate-800 hover:text-purple-700">Web Development</h4>
                 <h4 className="font-medium text-lg md:text-2xl underline text-slate-800 hover:text-purple-700">Mobile Development</h4>
                 <h4 className="font-medium text-lg md:text-2xl underline text-slate-800 hover:text-purple-700">Cloud Computing</h4>
                 <h4 className="font-medium text-lg md:text-2xl underline text-slate-800 hover:text-purple-700">Startup Services</h4>
             </div>
-            <div className="flex flex-col gap-3  w-1/2 h-full items-start pl-10">
+            <div className="flex flex-col gap-3  w-1/2 h-full items-start ml-0 md:pl-10">
                 <h4 className="font-medium text-lg md:text-2xl underline text-slate-800 hover:text-purple-700">Blockchain</h4>
                 <h4 className="font-medium text-lg md:text-2xl underline text-slate-800 hover:text-purple-700">Artificial Intelligence</h4>
                 <h4 className="font-medium text-lg md:text-2xl underline text-slate-800 hover:text-purple-700">ERP Consulting</h4>
