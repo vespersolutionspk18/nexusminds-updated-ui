78fe0b5e0e51faab3fc392981de6ee22b9708c1b
Vesper Solutions <emdc.aws.24@gmail.com>
2024-11-04 01:06:26 +0500

    fixed services 3

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,34 +1,32 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import SerTabs from './SerTabs';
-import { useSearchParams } from 'next/navigation';
+import dynamic from 'next/dynamic';
 import Header from '../Header';
 import Footer from '../Footer';
 import ContactSection from '../ContactSection';
 import SerHero from './SerHero';
 
-const ClientOnlyTabs = () => {
-  const searchParams = useSearchParams();
+// Dynamically import SerTabs to prevent it from being rendered on the server
+const SerTabs = dynamic(() => import('./SerTabs'), { ssr: false });
+
+const Page = () => {
   const [tabId, setTabId] = useState<string>('software-development');
 
   useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
     const tab = searchParams.get('tab');
     if (tab) {
       setTabId(tab);
     }
-  }, [searchParams]);
+  }, []);
 
-  return <SerTabs tabId={tabId} />;
-};
-
-const Page = () => {
   return (
     <div>
       <Header />
       <SerHero />
-      {/* Render the ClientOnlyTabs only on the client-side */}
-      <ClientOnlyTabs />
+      {/* Render SerTabs dynamically to ensure client-side rendering */}
+      <SerTabs tabId={tabId} />
       <ContactSection />
       <Footer />
     </div>
